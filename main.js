2a7b54f2e7c4b824fdb2868b192e2f2fc88904b0
Caleb Nash <calebnash@Calebs-MBP.attlocal.net>
2020-09-23 20:33:52 -0400

    images dont come in

---
+++ main.js
@@ -0,0 +1,78 @@
+const REPO_URL = 'https://api.github.com/users/CalebNash/repos';
+const USER_URL = 'https://api.github.com/users/CalebNash';
+const ORGS_URL = 'https://api.github.com/users/CalebNash/orgs';
+
+
+fetch(REPO_URL)
+  .then(response => response.json())
+  .then(res => {
+    console.log(res);
+    buildRepos(res);
+  })
+  .catch(error => console.log(error));
+
+function buildRepos(array) {
+  var source = document.getElementById("repo-template").innerHTML;
+  // console.log('source', source);
+  var template = Handlebars.compile(source);
+  // console.log(template);
+
+  var context = {
+    repos: array,
+  };
+
+  var html = template(context);
+  console.log(html);
+
+  document.querySelector('.repositories').innerHTML = html;
+}
+
+
+
+fetch(USER_URL)
+  .then(response => response.json())
+  .then(res => {
+    console.log(res);
+    buildUser(res);
+  })
+  .catch(error => console.log(error));
+
+  function buildUser(user) {
+    var source = document.getElementById("user-template").innerHTML;
+    // console.log('source', source);
+    var template = Handlebars.compile(source);
+    // console.log(template);
+
+    var context = {
+      repos: user,
+    };
+
+    var html = template(context);
+    console.log(html);
+
+    document.querySelector('.profile').innerHTML = html;
+  }
+
+  fetch(ORGS_URL)
+    .then(response => response.json())
+    .then(res => {
+      console.log(res);
+      buildOrgs(res);
+    })
+    .catch(error => console.log(error));
+
+    function buildOrgs(orgs) {
+      var source = document.getElementById("orgs-template").innerHTML;
+      // console.log('source', source);
+      var template = Handlebars.compile(source);
+      // console.log(template);
+
+      var context = {
+        repos: orgs,
+      };
+
+      var html = template(context);
+      console.log(html);
+
+      document.querySelector('.organizations').innerHTML = html;
+    }
